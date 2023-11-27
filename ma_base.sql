a4203684d99ce84a703f9ff33516fbc0fd702252
Ameni <kallel.ameni@gmail.com>
2023-11-27 10:40:34 +0100

    Update ma_base.sql

--- ma_base.sql
+++ ma_base.sql
@@ -4,3 +4,8 @@ CREATE TABLE IF NOT EXISTS utilisateurs (
     nom TEXT NOT NULL,
     email TEXT NOT NULL
 );
+-- insert_data.sql
+INSERT INTO utilisateurs (nom, email) VALUES
+('John Doe', 'john.doe@email.com'),
+('Jane Smith', 'jane.smith@email.com'),
+('Alice Wonderland', 'alice.wonderland@email.com');
